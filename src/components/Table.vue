736ead6a71fadc3dadd4095e60fb9855e113ebc5
etimarky <64045477+etimarky@users.noreply.github.com>
2021-10-27 22:20:44 -0400

    changes to vue table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,31 +1,46 @@
 <template>
-   <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-</table>
+  <table>
+    <tbody>
+      <tr>
+        <th scope = "col" v-for="h in headers" v-bind:key="h.id">{{ h }}</th>
+      </tr>
+      <div v-for="i in info" v-bind:key="i.id">
+        <tr v-for="j in i" v-bind:key="j.id">
+          <td>{{ j.title }}</td>
+          <td>{{ j.firstAirYear }}</td>
+          <td>{{ j.imdbID }}</td>
+          <td>{{ j.tmdbID }}</td>
+          <td>{{ j.imdbRating }}</td>
+          <td>{{ j.countries }}</td>
+          <td>{{ j.year }}</td>
+        </tr>
+      </div>
+    </tbody>
+  </table>
 </template>
 
 
 <script>
 export default {
-  name: 'MyTable',
+  name: "MyTable",
   props: {
+    info: Object,
     headers: Array,
-    items: Array
-  }
-}
+  },
+};
 </script>
+
+<style scoped>
+table {
+  width: 100%;
+}
+
+th,
+tr,
+td {
+  padding: 15px 0;
+  text-align: center;
+  border: 1px dotted gray;
+}
+</style>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Movie Logo" src="../assets/movie-logo.png" height="200px" width="700px">
-    <MovieApp msg="Welcome to the Movie Searching App"/>
+    <img
+      alt="Movie Logo"
+      src="../assets/movie-logo.png"
+      height="200px"
+      width="700px"
+    />
+    <MovieApp
+      msg="Welcome to the Movie Searching App"
+      :data="info"
+      :headers="headers"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import MovieApp from '@/components/MovieApp.vue'
+import MovieApp from "@/components/MovieApp.vue";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data: function () {
+    return {
+      info: {},
+      headers: Array,
+    };
+  },
+  // props: {
+  //   headers:
+  // },
   components: {
-    MovieApp
-  }
-}
+    MovieApp,
+  },
+  async mounted() {
+    let res = await axios.get("http://localhost:8080/");
+    this.headers = [
+      "title",
+      "first year aired",
+      "imdbID",
+      "tmdbID",
+      "imdbRating",
+      "countries",
+      "year",
+    ];
+    return (this.info = res.data);
+  },
+};
 </script>
